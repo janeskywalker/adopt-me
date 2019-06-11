97a28dfb7d3a1dc64022a20064cde30462944dc6
Jane Skywalker <janehuizeng@gmail.com>
2019-06-11 16:45:03 -0700

    creating a searchParams component

--- src/App.js
+++ src/App.js
@@ -35,9 +35,7 @@ const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SearchParams = () => {
-  const locaton = "Seattle, WA";
+  const location = "Seattle, WA";
 
   return (
     <div className="search-params">
