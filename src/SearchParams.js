55cc4f7fe7ad519a673126696c8a9ed795112810
Jane Skywalker <janehuizeng@gmail.com>
2019-06-11 17:43:25 -0700

    creating a hook useState for location and animal

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  // this is a hook - useState - stateful logic with react
+  // useState always gives back an array
+  // hooks can not go inside of conditions or loops
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
       <form>
         <lable htmlFor="location">
           location
-          <input id="location" value={location} placeholder="location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            // onChange, call setLocation
+            onChange={evt => setLocation(evt.target.value)}
+          />
         </lable>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => updateAnimal(e.target.value)}
+            onBlur={e => updateAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
         <button>Submit</button>
       </form>
     </div>
